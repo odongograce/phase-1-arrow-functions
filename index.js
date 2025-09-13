3f921a3b63d8032e77baba287e7b64352e925c62
Grace Odongo <graceakinyi024@gmail.com>
2025-09-13 11:13:38 +0300

    updates index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide=function(){
+    let a =2000
+    let b =100
+    return a/b;
+}
+const square=x=>x*x
+let x = 2
+const add=(a,b)=>a+b
+let a = 3
+let b=4
+console.log(divide());
+console.log(square());
+console.log(add());
